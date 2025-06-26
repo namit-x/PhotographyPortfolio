3f9b3b3bac95841aa2acc7c64830e6d11bff588f
namit-x <23btrca037@jainuniversity.ac.in>
2025-06-26 23:09:13 +0530

    again updated the configs

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     emptyOutDir: true,
-    chunkSizeWarningLimit: 3000 // Needed for Three.js
+    chunkSizeWarningLimit: 3000
   },
 })
